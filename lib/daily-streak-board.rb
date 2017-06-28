130da751f7f0e14b5f609ffadb79cb050d138b19
veloblank <veloblank@gmail.com>
2017-06-28 00:44:56 -0400

    #Initialize for DailyStreak with attr_accessors.

--- lib/daily-streak-board.rb
+++ lib/daily-streak-board.rb
@@ -1,6 +1,11 @@
 class DailyStreak
-  #has many props
-  
+  #has a date/day of week, has an array of many props, not responsible for scraping
+  attr_accessor :day, :props
+  def initialize(day)
+    @day = day
+    @props = [] #array or hash? probably array of props. props might be hashes.
+  end
+
 
 
 end
