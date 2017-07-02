742600e954f489df8cbc29702761156d39501346
veloblank <veloblank@gmail.com>
2017-07-02 03:01:12 -0400

    Very pleased. Got all props to populate and return with each_with_index.map!

--- lib/Prop.rb
+++ lib/Prop.rb
@@ -1,18 +1,13 @@
 require 'pry'
 
 class Prop
-  attr_accessor :title, :time, :sport, :away_team, :home_team, :preview_link, :away_team_url, :home_team_url
-  @@all = []
-
-  def initialize(array)
-    @title = title
+  attr_accessor :title, :time, :sport, :away_team, :home_team, :preview_link
     @time = time
     @sport = sport
     @away_team = away_team
     @home_team = home_team
     @preview_link = preview_link
-    @away_team_url = away_team_url
-    @home_team_url = home_team_url
+
     @@all << self
   end
 
--- lib/Scraper.rb
+++ lib/Scraper.rb
@@ -4,31 +4,40 @@ require 'pry'
 
 class Scraper
   ESPN = "http://streak.espn.com/en/"
-  @@all_props = {}
-  @@prop_key_titles = []
   @doc = Nokogiri::HTML(open(ESPN))
 
   def self.scrape
+    @@all_props = {}
+    @@prop_key_titles = []
     number_of_props = @doc.css("div.matchup-container").size
     prop_num = @doc.css("div.matchup-container").size
     prop_key = Array.new(prop_num, "Prop_")
-
+    binding.pry
     prop_key.each.with_index(1) do |prop, i|
-        @@prop_key_titles << "#{prop}"+"#{i}"
-        @@all_props = {
-        @@prop_key_titles[i-1] => {
-          event_title: @doc.css("div.matchup-container div.gamequestion strong")[0].text,
-          start_time: @doc.css("div.matchupDate")[0].text,
-          sport: @doc.css("div.sport-description")[0].text,
-          away_team: @doc.css("div #games-content tr td.mg-column3.opponents")[0].text,
-          home_team: @doc.css("div #games-content tr td.mg-column3.opponents.last")[0].text,
-          prop_preview: @doc.css("div.matchupStatus a")[0].attr("href")
-       }.reject {|k,v| k = :sport if @doc.css("div.sport-description").size != number_of_props}}
+      @@prop_key_titles << "#{prop}"+"#{i}"
     end
+
+      prop = @@prop_key_titles.each_with_index.map do |title, i|
+      @@all_props = {
+           "#{title}" => {
+          event_title: @doc.css("div.matchup-container div.gamequestion strong")[i].text,
+          start_time: @doc.css("div.matchupDate")[i].text,
+          sport: @doc.css("div.sport-description")[i].text,
+          away_team: @doc.css("div #games-content tr td.mg-column3.opponents")[i].text,
+          home_team: @doc.css("div #games-content tr td.mg-column3.opponents.last")[i].text,
+          prop_preview: @doc.css("div.matchupStatus a")[i].attr("href")
+       }.reject {|k,v| k = :sport if @doc.css("div.sport-description").size != number_of_props}}
+      end
   end
   binding.pry
 end
 
+#I am close to being finished with the scraper. I have had a lot of learn-on-the-fly moments--
+#with trying to remember hashes, how to write to them without overwriting their info, etc.
 
+#I think this is my problem now. The last pry showed it arrived at prop 21 with all the right info intact,
+#but not in a large nested array. I will tackle this later and move to the Prop class.
 
-# prop_title_count = "Prop_#{z}"
+#Got it!!!!!!
+
+#All 21 props but information is a little wonky.
