ff49f8f2a99c966795dd4d044e95799eb8cc5352
veloblank <veloblank@gmail.com>
2017-07-01 21:43:39 -0400

    Finalllygit add .! Workable scraper codegit add . Hash creation, variable cleanup, method cleanup.

--- lib/Scraper.rb
+++ lib/Scraper.rb
@@ -4,50 +4,52 @@ require 'pry'
 
 class Scraper
   ESPN = "http://streak.espn.com/en/"
-  @prop_hash = {}
-  @prop_keys = []
+  @@prop_hash = {}
+  @@prop_keys_array = []
+  @doc = Nokogiri::HTML(open(ESPN))
 
-  attr_accessor :number_of_props, :prop_keys
+  attr_accessor :number_of_props, :prop_keys_array
 
-  def initialize(number_of_props)
-    @number_of_props = number_of_props
+  def self.scrape
+    number_of_props = @doc.css("div.matchup-container").size
     i = 1
-    while i <= number_of_props do
-      @prop_keys << "Prop_#{i}:"
-      i+=1
+    while i <= number_of_props
+      @@prop_keys_array << "Prop_#{i}"
+       if i.even?
+         @@prop_hash = {
+           event_title: @doc.css("div.matchup-container div.gamequestion strong")[i].text,
+           start_time: @doc.css("div.matchupDate")[i].text,
+           prop_preview: @doc.css("div.matchupStatus a")[i].attr("href"),
+           away_team: @doc.css("div #games-content tr td.mg-column3.opponents")[i].text,
+           home_team: @doc.css("div #games-content tr td.mg-column3.opponents.last")[i].text}
+
+
+       end
+    i+=1
     end
-  end #=> "["Prop_1:", "Prop_2:", "Prop_3:"]"
+  end
+  binding.pry
+end
 
-  def self.scrape(prop_keys)
-    @prop_date = DateTime.now.strftime "%Y%m%d"
-    @doc = Nokogiri::HTML(open(ESPN))
 
-    prop_keys.each_with_index do |prop, i|
-     if i.even?
-       prop = {
-         event_title: @doc.css("div.matchup-container div.gamequestion strong")[i].text,
-         start_time: @doc.css("div.matchupDate")[i].text},
-         prop_preview: @doc.css("div.matchupStatus a")[i].attr("href"),
-         away_team: @doc.css("div #games-content tr td.mg-column3.opponents")[i].text},
-         home_team: @doc.css("div #games-content tr td.mg-column3.opponents.last")[i].text},
-             else
-          team_url = @doc.css("a#matchupDiv.mg-check.mg-checkEmpty.requireLogin")[i] do |team_link|
-            partial_url = team_url.attr("href")
-            if partial_url.split(%r{\s*}).last.to_i.even?
-            full_url = ESPN + partial_url + "&date=" + "@prop_date"
-              away_team_url: full_url,
-              home_team_url: full_url
-#             end
-#           }
-#       end
-#     end
-
-elsif i.odd?
+#          team_url = @doc.css("a#matchupDiv.mg-check.mg-checkEmpty.requireLogin")[i] do |team_link|
+#             partial_url = team_url.attr("href")
+#             if partial_url.split(%r{\s*}).last.to_i.even?
+#             full_url = ESPN + partial_url + "&date=" + "@prop_date"
+#               away_team_url: full_url,
+#               home_team_url: full_url
+# #             end
+# #           }
+# #       end
+# #     end
+#
+# elsif i.odd?
 
 #  def number_of_props
 #    number_of_props = @doc.css("div.matchup-container").size
 #  end
 
+# @prop_date = DateTime.now.strftime "%Y%m%d"
 
 
 
