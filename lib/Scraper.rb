582322c2802c5d629ef7e5828d3596c376553f84
veloblank <veloblank@gmail.com>
2017-07-02 00:25:52 -0400

    Added reject conditional for sport key if not all sport names can be scraped.

--- lib/Scraper.rb
+++ lib/Scraper.rb
@@ -12,25 +12,21 @@ class Scraper
 
   def self.scrape
     i = 0
+    z = 1
     number_of_props = @doc.css("div.matchup-container").size
-    while i < number_of_props do
-      @@prop_keys_array << "Prop_#{i+1}"
+    while i <= number_of_props do
+       @@prop_keys_array << "Prop_#{z}"
        @@prop_hash = {
-         event_title: @doc.css("div.matchup-container div.gamequestion strong")[i].text,
-         start_time: @doc.css("div.matchupDate")[i].text,
-         sport: nil,
-         away_team: @doc.css("div #games-content tr td.mg-column3.opponents")[i].text,
-         home_team: @doc.css("div #games-content tr td.mg-column3.opponents.last")[i].text,
-         prop_preview: @doc.css("div.matchupStatus a")[i].attr("href")}
-         team_url = @doc.css("a#matchupDiv.mg-check.mg-checkEmpty.requireLogin")[i]
-         partial_url = team_url.attr("href")
-         full_url = ESPN + partial_url + "&date=" + "@prop_date"
-         if i.even?
-           @@prop_hash[:away_team_url] = full_url
-         else
-           @@prop_hash[:home_team_url] = full_url
-         end
-    i+=1
+         event_title: @doc.css("div.matchup-container div.gamequestion strong")[0].text,
+         start_time: @doc.css("div.matchupDate")[0].text,
+         sport: @doc.css("div.sport-description")[0].text,
+         away_team: @doc.css("div #games-content tr td.mg-column3.opponents")[0].text,
+         home_team: @doc.css("div #games-content tr td.mg-column3.opponents.last")[0].text,
+         prop_preview: @doc.css("div.matchupStatus a")[0].attr("href")
+       }.reject {|k,v| k = :sport if @doc.css("div.sport-description").size != number_of_props}
+         binding.pry
+      z+=1
+      i+=1
     end
   end
   binding.pry
