086d443e87b4b9d235dbff6288c407dab185f20d
veloblank <veloblank@gmail.com>
2017-07-05 02:30:44 -0400

    Added URL to potentially make pick. Exciting feature!

--- lib/Scraper.rb
+++ lib/Scraper.rb
@@ -5,20 +5,23 @@ require 'pry'
 class Scraper
   ESPN = "http://streak.espn.com/en/"
   @@scraped_props = []
+  @@team_urls = []
 
   def self.scrape_site
-    away_teams = []
-    home_teams = []
-    formatted_date  = DateTime.now.strftime "%Y%m%d"
     @doc = Nokogiri::HTML(open(ESPN))
     prop_num = @doc.css("div.matchupDate").size
+    away_teams = []
+    home_teams = []
+
     @doc.css("div #games-content tr td.mg-column3.opponents").each_with_index do |x, i|
+      team_url = ESPN + "#{@doc.css("td a#matchupDiv.mg-check.mg-checkEmpty.requireLogin")[i].attr("href")}"
       if i.even?
-        away_teams.push [x.text]
+        away_teams.push [x.text, team_url]
       else
-        home_teams.push [x.text]
+        home_teams.push [x.text, team_url]
       end
     end
+
     i = 0
     while i < prop_num
       event = @doc.css("div.matchup-container div.gamequestion strong")[i].text
@@ -41,20 +44,14 @@ class Scraper
   def self.scraped_props
     @@scraped_props
   end
-binding.pry
+  binding.pry
 end
 
-#contains both home and away selection_urls
-@doc.css("td a#matchupDiv.mg-check.mg-checkEmpty.requireLogin")[index].attr("href")
- #=> "createOrUpdateEntry?matchup=m60175o61055&date=20170705"
 
- @doc.css("td a#matchupDiv.mg-check.mg-checkEmpty.requireLogin")[index].attr("selectionid")
-#=> "m60175o61055"
 
-#away_team
-@doc.css("td a#matchupDiv.mg-check.mg-checkEmpty.requireLogin")[1].attr("selectionid").split("").last.to_i.even?
-#=> false
 
-#home_team
-@doc.css("td a#matchupDiv.mg-check.mg-checkEmpty.requireLogin")[1].attr("selectionid").split("").last.to_i.even?
-#=> true
+
+
+
+
+#home_or_away =  @doc.css("td a#matchupDiv.mg-check.mg-checkEmpty.requireLogin")[index].attr("selectionid").split("").last.to_i
