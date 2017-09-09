398a4e541f8b7fc8df5e727b7cc998a9b5c262a3
veloblank <veloblank@gmail.com>
2017-09-09 08:09:15 -0400

    Saving and trying to figure out nil error when prop starts

--- lib/cli.rb
+++ lib/cli.rb
@@ -4,6 +4,7 @@ require 'time'
 require 'launchy'
 
 class Cli
+  @@user_selections = []
 
   def run
     Scraper.scrape_page
@@ -11,6 +12,7 @@ class Cli
     Prop.build_props
     self.welcome
     self.print_props
+    self.menu
   end
 
   def welcome
@@ -23,11 +25,41 @@ class Cli
     puts
   end
 
+  # if no props have started for the day, this prints a list of all props
   def print_props
     Prop.all_props.each do |prop|
       puts "#{prop.event_title}".colorize(:red)
       puts "#{prop.prop_id_num}. ".colorize(:green) + "#{prop.start_time}   " + "#{prop.sport}   " + "#{prop.away_team}   " + " vs. " + "   #{prop.home_team}"
       puts ""
+    end
+  end
+
+  def menu
+    input = nil
+    while input != "exit"
+      puts "Choose a prop you want to see more about:"
+      input = gets.strip.downcase
+      menu if user_input.to_i > Prop.all_props.size
+      case user_input
+      when "exit"
+        exit
+      else
+        prop = Prop.all_props[input-1]
+        puts "More info on:"
+        puts "#{prop.event_title}".colorize(:red)
+        puts "#{prop.prop_id_num}. ".colorize(:green) + "#{prop.start_time}   " + "#{prop.sport}   " + "#{prop.away_team}   " + " vs. " + "   #{prop.home_team}"
+        puts ""
+        puts ""
       end
     end
+  end
+
+  def user_selections
+    @@user_selections.each do |prop|
+      puts "#{prop.event_title}".colorize(:red)
+      puts "#{prop.start_time}   " + "#{prop.sport}   " + "#{prop.away_team}   " + " vs. " + "   #{prop.home_team}"
+      puts ""
+      puts ""
+    end
+  end
 end
--- lib/scraper.rb
+++ lib/scraper.rb
@@ -15,6 +15,7 @@ class Scraper
 
   def self.scrape_props
     @@scraped_props.each_with_index do |p, index|
+      binding.pry
       prop = {
         prop_id_num: index + 1,
         event_title: p.css(".gamequestion").text,
@@ -23,9 +24,11 @@ class Scraper
         away_team: p.css("td span strong")[0].text,
         home_team: "@" + p.css("td span strong")[1].text,
         prop_preview: p.css("div.matchupStatus a").attr("href").value,
+
         away_team_url: "http://streak.espn.com/en/" + p.css("td a#matchupDiv.mg-check.mg-checkEmpty.requireLogin")[0].attr("href"),
         home_team_url: "http://streak.espn.com/en/" + p.css("td a#matchupDiv.mg-check.mg-checkEmpty.requireLogin")[1].attr("href")
       }
+
         # CSS issue if one or both of opponents are ranked (i.e "#20 Auburn" steals one of the 'strong' tags and won't properly seed prop opponents from array
         if p.css("td span strong").size == 4
           prop[:away_team] = "#{p.css("td span strong")[0].text}" + "#{p.css("td span strong")[1].text}"
@@ -46,4 +49,6 @@ class Scraper
   def self.all_props
     @@props
   end
+
+  binding.pry
 end
