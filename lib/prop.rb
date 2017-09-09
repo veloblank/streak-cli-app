fd108e79e56c46949719e55fb2d4d19309320cc7
veloblank <veloblank@gmail.com>
2017-09-08 22:42:52 -0400

    Totally refactored scraper and Prop class

--- lib/prop.rb
+++ lib/prop.rb
@@ -2,23 +2,40 @@ require 'pry'
 
 class Prop
   @@all = []
+  @@props = []
 
-  attr_accessor :event_title, :start_time, :sport, :away_team, :home_team, :prop_preview, :away_team_url, :home_team_url
+  attr_accessor :prop_id_num, :event_title, :start_time, :sport, :away_team, :home_team, :prop_preview, :away_team_url, :home_team_url
 
   def initialize(prop)
-    prop.each do |k, v|
-      self.send("#{k}=", "#{v}")
-    end
-    @@all << self
+
   end
 
-  def self.generate_props_by_hash(props_hash)
-    Scraper.scrape_team_urls
-    props_hash.each_with_index do |prop, i|
-      prop[:away_team_url] = Scraper.away_team_urls[i]
-      prop[:home_team_url] = Scraper.home_team_urls[i]
-      Prop.new(prop)
+  def self.get_props
+    @@all = []
+    all_props = Scraper.get_page
+    all_props.each do |prop|
+      @@all << prop
     end
+    @@all.size
+  end
+
+  def self.build_props
+    @@all.each.with_index(1) do |p, index|
+      prop = {
+       prop_id_num: index,
+       event_title: p.css(".gamequestion").text,
+       start_time: p.css("div .startTime").text,
+       sport: p.css(".sport-description").text,
+       away_team: p.css("td span")[2].text,
+       home_team: p.css("td span")[5].text,
+       prop_preview: p.css("div.matchupStatus a").attr("href").value,
+       away_team_url: "http://streak.espn.com/en/" + p.css("td a#matchupDiv.mg-check.mg-checkEmpty.requireLogin")[0].attr("href"),
+       home_team_url: "http://streak.espn.com/en/" + p.css("td a#matchupDiv.mg-check.mg-checkEmpty.requireLogin")[1].attr("href")
+     }
+     @@props << prop
+    end
+    binding.pry
+
   end
 
   def self.all
--- lib/scraper.rb
+++ lib/scraper.rb
@@ -5,69 +5,13 @@ require 'pry'
 
 class Scraper
   ESPN = "http://streak.espn.com/en/"
-  @@scraped_props = []
-  @@team_urls = []
-  @@away_team_urls = []
-  @@home_team_urls = []
 
   def self.get_page
     @doc = Nokogiri::HTML(open(ESPN))
-    @prop_num = @doc.css("div.matchupDate").size
-  end
-
-  def self.get_teams
-    @away_teams = []
-    @home_teams = []
-    @doc.css("div #games-content tr td.mg-column3.opponents").each_with_index do |team, index|
-      index.even? ? @away_teams.push(team.text) : @home_teams.push(team.text)
-    end
-  end
-
-  def self.scrape_team_urls
-    team_urls = []
-      if @doc.css("td a#matchupDiv.mg-check.mg-checkEmpty.requireLogin").size/2 == @@scraped_props.size
-        @doc.css("td a#matchupDiv.mg-check.mg-checkEmpty.requireLogin").each_with_index do |team, index|
-          team_url = ESPN + "#{team.attr("href")}"
-          team_urls << team_url
-        end
-      else
-        team_urls = Array.new(@prop_num, "http://blank.org")
-      end
-    @@team_urls = team_urls.partition.with_index{|url, i| i.even?}
-    @@away_team_urls = @@team_urls[0]
-    @@home_team_urls = @@team_urls[1]
-  end
-
-  def self.build_props
-    get_teams
-    @doc.css("div.matchup-container div.gamequestion strong").each_with_index do |event, index|
-      event = event.text
-      start = Time.parse(@doc.css("div.matchupDate")[index].text)
-      sport = @doc.css("div.sport-description")[index].text
-      prop_preview = @doc.css("div.matchupStatus a")[index].attr("href")
-      prop = {
-        event_title: event,
-        start_time: start.strftime("%H:%M"),
-        sport: sport,
-        prop_preview: prop_preview,
-        away_team: @away_teams[index],
-        home_team: @home_teams[index],
-        away_team_url: @@away_team_urls[index],
-        home_team_url: @@home_team_urls[index]
-        }
-        @@scraped_props << prop
-    end
+    @@scraped_props = @doc.css("div .matchup-container")
   end
 
   def self.all_props
     @@scraped_props
-  end
-
-  def self.home_team_urls
-    @@home_team_urls
-  end
-
-  def self.away_team_urls
-    @@away_team_urls
   end
 end
